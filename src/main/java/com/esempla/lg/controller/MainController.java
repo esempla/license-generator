b4fae92280b17156b2f7cc427e7afa6014f2e19b
MihaiGaidau <gaidaumihai@gmail.com>
2020-01-07 00:28:54 +0200

    button upload is working

--- src/main/java/com/esempla/lg/controller/MainController.java
+++ src/main/java/com/esempla/lg/controller/MainController.java
@@ -2,6 +2,7 @@ package com.esempla.lg.controller;
 
 import com.esempla.lg.Launcher;
 import com.esempla.lg.model.Key;
+import com.esempla.lg.service.FilesManager;
 import com.esempla.lg.service.KeyManager;
 import com.esempla.lg.util.FXMLLoaderProvider;
 import com.esempla.lg.util.FileSystemUtil;
@@ -11,11 +12,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
 import java.io.IOException;
 
 @Slf4j
@@ -24,9 +28,13 @@ public class MainController extends AbstractController{
     private KeyStorage keyStorage;
     private FileSystemUtil fileSystemUtil;
     private KeyManager keyManager;
+    private FilesManager filesManager;
 
     @FXML
     private ListView<Key> keysListView;
+
+    @FXML
+    private TextArea licenseTextArea;
 
     public MainController() {
         super(new FXMLLoaderProvider());
@@ -34,6 +42,7 @@ public class MainController extends AbstractController{
         this.keyStorage = KeyStorage.getInstance();
         this.fileSystemUtil = new FileSystemUtil();
         this.keyManager = new KeyManager();
+        this.filesManager = new FilesManager();
     }
 
     @FXML
@@ -58,4 +67,26 @@ public class MainController extends AbstractController{
         dialogStage.setScene(scene);
         dialogStage.showAndWait();
     }
+
+    @FXML
+    void handleUploadButton(ActionEvent event) {
+
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            log.info(file.getName().toString());
+        }
+        String data = filesManager.readFromFile(file);
+        if (data != null){
+            log.info("the "+file.getName()+" contains: \n"+ data);
+            log.info("set text for licenseTextArea:");
+            licenseTextArea.textProperty().set(data);
+        }
+        else{
+            log.warn("The file: "+file.getName()+" is empty");
+        }
+    }
+
+
+
 }
